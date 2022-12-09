811c5caa32f371a0d05d569420cea9ab1bf0bcec
dmytrotrydub <tridub.d87@gmail.com>
2022-12-08 22:10:04 -0500

    posting into a dashboard functionality is working(in progress)

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,86 @@
+import ToDoItem from '../ToDoItem/ToDoItem';
+import axios from 'axios';
+
+import { useEffect, useState } from 'react';
+
+const Dashboard = () => {
+  const [listOfItems, setListOfItems] = useState([]);
+  const [newItem, setNewItem] = useState(null);
+  const [newItemData, setNewItemData] = useState({ Description: null });
+
+  const [taskInputForm, setTaskInputForm] = useState(false);
+
+  const handleItemData = (e) => {
+    // e.preventDefault();
+    // console.log(e.target.children[1].value);
+
+    postTaskIntoDB(newItem);
+    setNewItem(newItemData);
+  };
+  const postTaskIntoDB = (item) => {
+    const header = {
+      'Content-Type': 'text',
+      charset: 'utf-8',
+    };
+
+    const postData = axios.post(
+      `${process.env.REACT_APP_SERVER}${process.env.REACT_APP_PORT}/`,
+      newItemData,
+      header
+    );
+
+    postData
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    const toDoData = axios.get(`${process.env.REACT_APP_SERVER}${process.env.REACT_APP_PORT}/`);
+
+    toDoData
+      .then((response) => {
+        setListOfItems(response.data);
+      })
+      .catch((error) => {
+        console.log(`Invalid data received : ${error}`);
+      });
+  }, [newItem]);
+
+  return (
+    <div>
+      <button
+        type='button'
+        onClick={(e) => {
+          setTaskInputForm(true);
+        }}
+        children={'ADD TASK'}
+      />
+      {taskInputForm === true ? (
+        <form onSubmit={(e) => handleItemData(e)}>
+          <label htmlFor='task-input-form'>ENTER TASK TO TRACK</label>
+          <input
+            type='text'
+            onChange={(e) => setNewItemData({ Description: e.target.value })}
+            id='task-input-form'
+            name='INPUT TASK'
+          />
+          <input type='submit' value='START TRACKING' />
+        </form>
+      ) : null}
+      {listOfItems.map((item) => {
+        return (
+          <ToDoItem
+            key={item.id}
+            id={item.id}
+            description={item.Description}
+            dateUpdated={item.dateUpdated}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/ToDoItem/ToDoItem.js
@@ -0,0 +1,9 @@
+
+
+const ToDoItem = (props) => {
+    return (<div>
+        <p>{props.description}</p>
+    </div>);
+};
+
+export default ToDoItem;
